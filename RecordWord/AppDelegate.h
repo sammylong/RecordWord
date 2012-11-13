0e5ec520bc26207949d0198f5db8a9fffbc4a746
Sammy Long <dragonbabe22@gmail.com>
2012-11-13 16:40:33 +0800

    Initial Commit

---
+++ RecordWord/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  RecordWord
+//
+//  Created by Sammy Long on 12/11/13.
+//  Copyright (c) 2012年 Apexlearn Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ RecordWord/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  RecordWord
+//
+//  Created by Sammy Long on 12/11/13.
+//  Copyright (c) 2012年 Apexlearn Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
